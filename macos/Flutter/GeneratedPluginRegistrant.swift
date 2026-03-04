d3d90fa6e49e502e3ded7c397c364918118b986a
Noctambulist007 <noctambulist007@gmail.com>
2026-03-04 12:46:07 +0600

    chore(deps): add flutter_local_notifications and alarm dependencies
    - Add flutter_local_notifications, flutter_timezone, and alarm packages to pubspec.yaml
    - Update pubspec.lock with resolved dependency tree
    - Regenerate plugin registrants for macOS and Windows

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,6 +7,8 @@ import Foundation
 
 import file_picker
 import file_selector_macos
+import flutter_local_notifications
+import flutter_timezone
 import package_info_plus
 import path_provider_foundation
 import shared_preferences_foundation
@@ -17,6 +19,8 @@ import webview_flutter_wkwebview
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
+  FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
+  FlutterTimezonePlugin.register(with: registry.registrar(forPlugin: "FlutterTimezonePlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
